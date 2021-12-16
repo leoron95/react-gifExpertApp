bd947d745e15536725bfd718326559e46703962e
Leobel Rondón <leobel95@gmail.com>
2021-12-16 05:44:24 -0400

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid'
+
+function GifExpertApp() {
+
+    const [categories, setCategories] = useState([''])
+
+    // const handleAdd = () => {
+    //     // setCategories([...categories, 'Music']);
+    //     setCategories(cate => [...cate, 'Music']);
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories= {setCategories}/>
+            <hr/>
+
+            
+
+            <ol>
+                {
+                    categories.map(category  => 
+                        <GifGrid
+                        key = {category}
+                        category = {category} />
+                    )
+                }
+            </ol>
+        </>
+    )
+}
+
+export default GifExpertApp
+
